8331e0c3a1292429a32454e50cf3b2fa1774bab9
Vladimir Shilin <vladimirshilin@me.com>
2026-02-27 01:57:25 +0300

    splfixedarray and math for converting date string to number, sockets

--- app/Parser.php
+++ app/Parser.php
@@ -16,27 +16,23 @@ final class Parser
 
     private const int COMMA_TO_NEWLINE_OFFSET = 27;
 
-    private const int WORKERS = 8;
+    private const int WORKERS = 12;
 
-    private function worker(string $filepath, string $inputPath, int $start, int $end): void
+    private function worker(\Socket $socket, string $inputPath, int $start, int $end): void
     {
         $result = $this->process($inputPath, $start, $end);
-        file_put_contents($filepath, igbinary_serialize($result));
+        $data = igbinary_serialize($result);
+        socket_write($socket, $data, strlen($data));
+        socket_close($socket);
     }
 
     private function process(string $inputPath, int $start, int $end): array
     {
-        $empty = [];
-        for ($y = 2021; $y <= 2026; $y++) {
-            for ($m = 1; $m <= 12; $m++) {
-                for ($d = 1; $d <= 31; $d++) {
-                    $empty[sprintf('%04d-%02d-%02d', $y, $m, $d)] = 0;
-                }
-            }
-        }
+        $empty = \SplFixedArray::fromArray(array_fill(0, 6 << 9, 0));
         $result = [];
 
         $handle = fopen($inputPath, 'r');
+        stream_set_read_buffer($handle, 0);
 
         fseek($handle, $start);
         $cur = $start;
@@ -58,9 +54,11 @@ final class Parser
                 }
                 $link = substr($data, $o + self::URL_FIXED_LENGTH, $nextComma - $o - self::URL_FIXED_LENGTH);
                 if (!isset($result[$link])) {
-                    $result[$link] = $empty;
+                    $result[$link] = clone $empty;
                 }
-                $date = substr($data, $nextComma + 1, self::DATE_LENGTH);
+                $date = (ord($data[$nextComma + 4]) << 9)
+                    + ((10 * ord($data[$nextComma + 6]) + ord($data[$nextComma + 7])) << 5)
+                    + 10 * ord($data[$nextComma + 9]) + ord($data[$nextComma + 10]) - 42512;
                 $result[$link][$date] += 1;
 
                 $o = $nextComma + self::COMMA_TO_NEWLINE_OFFSET;
@@ -73,6 +71,8 @@ final class Parser
             $end += 2;
             goto fixnewlineatend;
         }
+
+        fclose($handle);
 
         return $result;
     }
@@ -100,15 +100,14 @@ final class Parser
         ob_start();
         $links = array_keys($res);
         $link = $links[0];
-        $dates = array_keys($res[$link]);
 
         echo "{\n";
         echo '    "\/blog\/' . str_replace('/', '\/', $link) . '": {' . "\n";
         $j = 0;
-        $jl = count($dates);
+        $jl = 6 << 9;
         while ($j < $jl) {
-            $date = $dates[$j];
-            $cnt = $res[$link][$date];
+            $cnt = $res[$link][$j];
+            $date = sprintf('%d-%02d-%02d', 2021 + ($j >> 9), ($j >> 5) % 16, $j % 32);
             if ($cnt == 0) {
                 $j++;
 
@@ -119,8 +118,8 @@ final class Parser
             break;
         }
         while ($j < $jl) {
-            $date = $dates[$j];
-            $cnt = $res[$link][$date];
+            $cnt = $res[$link][$j];
+            $date = sprintf('%d-%02d-%02d', 2021 + ($j >> 9), ($j >> 5) % 16, $j % 32);
             if ($cnt == 0) {
                 $j++;
 
@@ -136,8 +135,8 @@ final class Parser
             echo ",\n" . '    "\/blog\/' . str_replace('/', '\/', $link) . '": {' . "\n";
             $j = 0;
             while ($j < $jl) {
-                $date = $dates[$j];
-                $cnt = $res[$link][$date];
+                $cnt = $res[$link][$j];
+                $date = sprintf('%d-%02d-%02d', 2021 + ($j >> 9), ($j >> 5) % 16, $j % 32);
                 if ($cnt == 0) {
                     $j++;
 
@@ -148,8 +147,8 @@ final class Parser
                 break;
             }
             while ($j < $jl) {
-                $date = $dates[$j];
-                $cnt = $res[$link][$date];
+                $cnt = $res[$link][$j];
+                $date = sprintf('%d-%02d-%02d', 2021 + ($j >> 9), ($j >> 5) % 16, $j % 32);
                 if ($cnt == 0) {
                     $j++;
 
@@ -176,40 +175,44 @@ final class Parser
         $chunk = intdiv($size, self::WORKERS) + 1;
         $start = -1;
 
-        $tempFiles = [];
-        for ($i = 0; $i < self::WORKERS; $i++) {
-            $tempFiles[] = sys_get_temp_dir() . '/parser_worker_' . $i . '.dat';
-            if (file_exists($tempFiles[$i])) {
-                @unlink($tempFiles[$i]);
-            }
-        }
-
-        $pids = [];
+        $sockets = [];
 
         for ($i = 0; $i < self::WORKERS; $i++) {
+            $socketPair = [];
+            socket_create_pair(AF_UNIX, SOCK_STREAM, 0, $socketPair);
+
             $pid = pcntl_fork();
             if ($pid == 0) {
-                $this->worker($tempFiles[$i], $inputPath, $start + 1, min($start + $chunk, $size));
+                socket_close($socketPair[1]);
+                $this->worker($socketPair[0], $inputPath, $start + 1, min($start + $chunk, $size));
                 exit(0);
             }
+            socket_close($socketPair[0]);
+            $sockets[] = $socketPair[1];
 
-            $pids[] = $pid;
             $start += $chunk;
-        }
-
-        foreach ($pids as $pid) {
-            pcntl_wait($pid);
         }
 
         $results = [];
 
-        foreach ($tempFiles as $i => $f) {
-            $data = @file_get_contents($f);
+        foreach ($sockets as $socket) {
+            $read = [$socket];
+            $write = null;
+            $expect = null;
+            socket_select($read, $write, $expect, null);
+            $data = '';
+            while (true) {
+                $chunk = socket_read($socket, 10 * 1024 * 1024, PHP_BINARY_READ);
+                if ($chunk === '' || $chunk === false) {
+                    break;
+                }
+                $data .= $chunk;
+            }
+            socket_close($socket);
             $results[] = $data ? igbinary_unserialize($data) : [];
-            @unlink($f);
         }
 
-        $res = self::merge($results);
+        $res = $this->merge($results);
         $this->writeResult($outputPath, $res);
     }
 }
